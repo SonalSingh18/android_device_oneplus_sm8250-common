c68cd6126e4e3588940ba07ea33e328610e32064
zenzer0s <praveenzalaki200@gmail.com>
2026-09-08 08:21:56 +0000

    sm8250-common: sepolicy: Silence harmless vold spunvm denial

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -150,6 +150,7 @@ include device/lineage/sepolicy/libion/sepolicy.mk
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include hardware/oplus/sepolicy/qti/SEPolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
